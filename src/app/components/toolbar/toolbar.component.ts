e5e4ae90c66cb20dfc30d79f2020a348e0f15af2
Guilermo1212 <80720696+Guilermo1212@users.noreply.github.com>
2023-11-19 06:31:23 -0500

    Update toolbar.component.ts

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -44,7 +44,11 @@ export class ToolbarComponent {
   redirectToRegister() {
     this.router.navigate(['/register']);
   }
-
+  
+  redirectToAddPublication() {
+    this.router.navigate(['/add-publication']);
+  }
+  
   logout() {
     localStorage.removeItem('user');
     this.router.navigate(['/login']).then(() => {
